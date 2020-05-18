5b64713d9373236a1c636f4c39df83d38a2f9882
werest <weresttrade@ya.ru>
2020-05-18 16:55:29 +0300

    Update task_1

--- task_1.sql
+++ task_1.sql
@@ -1,10 +1,10 @@
 select
-    concat_ws(' ', FirstName, MidName, LastName) as ФИО
+    concat_ws(' ', FirstName, MidName, LastName) as 'Полное ФИО пациента'
+    ,month(Date) as Месяц
     ,D.Name as Диагноз
-    ,substring(convert(varchar, Date, 23), 6, 2) as Месяц
-    ,COUNT(D.Name) as 'Количество посещений'
+    ,COUNT(D.Name) as 'Количество посещений за месяц по текущему диагнозу'
 from Patients
 join Schedules S on Patients.Id = S.PatientId
 join Diagnoses D on S.DiagnosisId = D.Id
-GROUP BY D.Name, concat_ws(' ', FirstName, MidName, LastName), substring(convert(varchar, Date, 23), 6, 2)
+GROUP BY D.Name, month(Date), concat_ws(' ', FirstName, MidName, LastName)
 HAVING COUNT(D.Name) > 1
