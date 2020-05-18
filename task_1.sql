e61ccc37ef2f83f6dba5264d372910876ab75ace
werest <weresttrade@ya.ru>
2020-05-18 17:43:22 +0300

    upd task 1

--- task_1.sql
+++ task_1.sql
@@ -2,9 +2,9 @@ select
     concat_ws(' ', FirstName, MidName, LastName) as 'Полное ФИО пациента'
     ,month(Date) as Месяц
     ,D.Name as Диагноз
-    ,COUNT(D.Name) as 'Количество посещений за месяц по текущему диагнозу'
+    ,COUNT(1) as 'Количество посещений за месяц по текущему диагнозу'
 from Patients
 join Schedules S on Patients.Id = S.PatientId
 join Diagnoses D on S.DiagnosisId = D.Id
 GROUP BY D.Name, month(Date), concat_ws(' ', FirstName, MidName, LastName)
-HAVING COUNT(D.Name) > 1
+HAVING COUNT(1) > 1
